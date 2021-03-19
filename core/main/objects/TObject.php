ecb4cdac0d7c28c88cb1a4a41d159ea95ed28284
silentwasd <silentwasd@gmail.com>
2021-03-19 06:17:03 +0500

    Refactored TObject class.

--- core/main/objects/TObject.php
+++ core/main/objects/TObject.php
@@ -10,49 +10,82 @@ class TObject extends _Object
     /** @var int Идентификатор. */
     public $self;
 
-    function __construct($init = true)
+    function __construct()
     {
         $this->self = component_create(__CLASS__, nil);
     }
 
-    function get_className()
+    /**
+     * Проверить объект на принадлежность классу/массиву классов.
+     * @param string|string[] $class Класс или массив классов.
+     * @return bool Если объект принадлежит классу/массиву классов, возвращается true, иначе false.
+     */
+    public function isClass($class)
     {
-        return rtii_class($this->self);
+        if (is_array($class))
+            return $this->checkClassInArray($class);
+
+        return strtolower($class) == strtolower($this->className);
     }
 
-    function isClass($class)
+    /**
+     * Безопасно удалить объект.
+     */
+    public function safeFree()
     {
-        if (is_array($class)) {
-            $s_class = strtolower($this->className);
-            foreach ($class as $el)
-                if (strtolower($el) == $s_class)
-                    return true;
-            return false;
-        } else {
-            $class = strtolower($class);
-            return $class == strtolower($this->className);
-        }
-    }
-
-    function safeFree()
-    {
-        if (class_exists('animate'))
-            animate::objectFree($this->self);
-
+        $this->animateFree();
         gui_safeDestroy($this->self);
     }
 
-    function destroy()
+    /**
+     * Удалить объект.
+     */
+    public function destroy()
     {
         $this->free();
     }
 
-    function free()
+    /**
+     * Удалить объект.
+     */
+    public function free()
+    {
+        $this->animateFree();
+        gui_destroy($this->self);
+    }
+
+    /**
+     * Проверить объект на принадлежность классу из массива.
+     * @param array $classes Массив классов.
+     * @return bool Если объект принадлежит хотя-бы одному классу из массива возвращается true, иначе false.
+     */
+    protected function checkClassInArray(array $classes)
+    {
+        $s_class = strtolower($this->className);
+
+        foreach ($classes as $el) {
+            if (strtolower($el) == $s_class)
+                return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Получить название класса.
+     * @return string
+     */
+    protected function get_className()
+    {
+        return rtii_class($this->self);
+    }
+
+    /**
+     * Удалить объект по-особому, если это объект класса "animate".
+     */
+    protected function animateFree()
     {
         if (class_exists('animate'))
             animate::objectFree($this->self);
-
-        gui_destroy($this->self);
-        //obj_free($this->self);
     }
 }
