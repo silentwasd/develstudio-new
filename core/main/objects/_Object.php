928a569ed0242b54384216bb46860a70ad4057d3
silentwasd <silentwasd@gmail.com>
2021-03-19 05:30:10 +0500

    Refactored _Object.

--- core/main/objects/_Object.php
+++ core/main/objects/_Object.php
@@ -1,54 +1,102 @@
 <?
 
 
+const OBJECT_PROPERTY_NOT_FOUND = -908067676;
+
 /**
  * Class for Object with property ala java
  */
 class _Object
 {
-
     protected $props = array();
     protected $class_name = __CLASS__;
 
-    function getAllProps()
+    public function __get($nm)
     {
-        return $this->props;
-    }
+        if ($this->getterMethod($nm, $result))
+            return $result;
 
-    function __get($nm)
-    {
-        $s = 'get_' . $nm;
-        $s2 = 'getx_' . $nm;
-        $isset = true;
-        if (method_exists($this, $s2)) {
-            return $this->$s2();
-        } elseif (method_exists($this, $s))
-            return $this->$s();
-        elseif (property_exists($this, $nm))
+        if (property_exists($this, $nm))
             return $this->$nm;
-        elseif (array_key_exists($nm, $this->props) && method_exists($this, 'setx_' . $nm)) {
-            return $this->__getPropEx($nm);
-        } elseif (array_key_exists($nm, $this->props)) {
-            return $this->props[$nm];
-        } else {
-            return -908067676;
-        }
+
+        if ($this->getterProps($nm, $result))
+            return $result;
+
+        return OBJECT_PROPERTY_NOT_FOUND;
     }
 
-    function __set($nm, $val)
+    public function __set($nm, $val)
     {
-
-        $s = 'set_' . $nm;
-        $s2 = 'setx_' . $nm;
         if (property_exists($this, $nm)) {
             $this->$nm = $val;
-        } elseif (method_exists($this, $s2)) {
-            $this->props[$nm] = $val;
+            return;
         }
 
-        if (method_exists($this, $s))
-            $this->$s($val);
-        if (method_exists($this, $s2))
-            $this->$s2($val);
+        if (method_exists($this, $this->setterPrefixedB($nm))) {
+            $this->props[$nm] = $val;
+            return;
+        }
+
+        $this->setterMethod($nm, $val);
+    }
+
+    private function getterPrefixedA($property)
+    {
+        return 'get_'.$property;
+    }
+
+    private function getterPrefixedB($property)
+    {
+        return 'getx_'.$property;
+    }
+
+    private function setterPrefixedA($property)
+    {
+        return 'set_'.$property;
+    }
+
+    private function setterPrefixedB($property)
+    {
+        return 'setx_'.$property;
+    }
+
+    private function getterMethod($property, &$result)
+    {
+        $prefixedA = $this->getterPrefixedA($property);
+        $prefixedB = $this->getterPrefixedB($property);
+
+        if (method_exists($this, $prefixedB))
+            $result = $this->$prefixedB();
+        elseif (method_exists($this, $prefixedA))
+            $result = $this->$prefixedA();
+        else
+            return false;
+
+        return true;
+    }
+
+    private function getterProps($property, &$result)
+    {
+        $existInProps = array_key_exists($property, $this->props);
+
+        if ($existInProps && method_exists($this, $this->setterPrefixedB($property)))
+            $result = $this->__getPropEx($property);
+        elseif ($existInProps)
+            $result = $this->props[$property];
+        else
+            return false;
+
+        return true;
+    }
+
+    private function setterMethod($property, $value)
+    {
+        $prefixedA = $this->setterPrefixedA($property);
+        $prefixedB = $this->setterPrefixedB($property);
+
+        if (method_exists($this, $prefixedA))
+            $this->$prefixedA($value);
+        elseif (method_exists($this, $prefixedB))
+            $this->$prefixedB($value);
     }
 }
